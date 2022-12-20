7685c4e8d0cac0dcf0d24c641791633a4b1768ef
Vinicius Loch <viniciusdrloch@gmail.com>
2022-12-19 21:22:56 -0300

    Terminando validação forget password

--- cypress/e2e/validarCredenciais.cy.js
+++ cypress/e2e/validarCredenciais.cy.js
@@ -1,9 +1,17 @@
 describe('Validar credenciais e forget your password', () => {
+  
   it('Validando mensagem credenciais incorretas', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get('input[name=username]').type('Admin1')
     cy.get('input[name=password]').type('admin123')
     cy.get('.oxd-button').click()
     cy.get('.oxd-alert-content').should('be.visible')
+  })
+
+  it('Validando forget yout password', () => {
+    cy.get('.orangehrm-login-forgot > .oxd-text').click()
+    cy.get('.oxd-input').type('Admin')
+    cy.get('.oxd-button--secondary').click()
+    cy.get('.oxd-text--h6').should('be.visible')
   })
 })
