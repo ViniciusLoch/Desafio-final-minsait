3a538c4160616d5bda6a4b67336db0387c83a171
Vinicius Loch <viniciusdrloch@gmail.com>
2022-12-27 13:46:04 -0300

    Validando Search and Admin

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      
+
     },
 
-    "chromeWebSecurity":false,
     
+    viewportWidth: 1440,
+    viewportHeight: 900
   },
 });
 
---
+++ cypress/e2e/adminAndSearch.cy.js
@@ -0,0 +1,26 @@
+/// <reference types= 'cypress'/>
+
+describe('Validando acesso ao admin, pesquisando usuario e excluindo', () => {
+    beforeEach(()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('input[name=username]').type('Admin')
+        cy.get('input[name=password]').type('admin123')
+        cy.get('.oxd-button').click()
+    })
+
+    it('Validando search', () => {
+        cy.get('.oxd-main-menu-search > .oxd-input').type('Admin')
+        cy.get('.oxd-main-menu-item > .oxd-text').should('be.visible')
+    })
+
+
+
+    it('Validando acesso ao admin', () => {
+        cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+        cy.get('.oxd-table-filter-header-title > .oxd-text').should('be.visible')
+    })
+
+    
+
+
+})
